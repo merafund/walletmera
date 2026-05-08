bc15a9654740b4de644ad140b849ff185152f914
ilau020203 <antip19052a016@gmail.com>
2026-05-08 16:18:05 +0300

    feat: add extensive tests for MERAWalletThresholdGuardian, covering proposal cancellation, execution errors, and constructor validations

--- test/MERAWalletThresholdGuardian.t.sol
+++ test/MERAWalletThresholdGuardian.t.sol
@@ -32,6 +32,82 @@ contract MERAWalletThresholdGuardianTest is Test {
         guardian.setWallet(address(wallet));
     }
 
+    function test_ActiveProposal_CancelledReverts() public {
+        bytes32 proposalId = _propose(address(0xE7001), 1 days, member1);
+        vm.prank(member1);
+        guardian.cancelProposal(proposalId);
+
+        vm.prank(member2);
+        vm.expectRevert(abi.encodeWithSelector(MERAWalletThresholdGuardian.ProposalIsCancelled.selector, proposalId));
+        guardian.approveProposal(proposalId);
+    }
+
+    function test_ActiveProposal_NotFoundReverts() public {
+        bytes32 badId = keccak256("nonexistent");
+        vm.expectRevert(abi.encodeWithSelector(MERAWalletThresholdGuardian.ProposalNotFound.selector, badId));
+        guardian.executeProposal(badId);
+    }
+
+    function test_Propose_PastDeadlineReverts() public {
+        vm.warp(1000);
+        vm.prank(member1);
+        vm.expectRevert(MERAWalletThresholdGuardian.InvalidDeadline.selector);
+        guardian.proposeEmergencyChange(address(0xE9999), uint64(block.timestamp));
+    }
+
+    function test_Propose_ZeroEmergencyReverts() public {
+        vm.prank(member1);
+        vm.expectRevert(MERAWalletThresholdGuardian.InvalidEmergency.selector);
+        guardian.proposeEmergencyChange(address(0), uint64(block.timestamp + 1 days));
+    }
+
+    function test_Propose_BeforeWalletSetReverts() public {
+        address[] memory members = new address[](1);
+        members[0] = member1;
+        MERAWalletThresholdGuardian g = new MERAWalletThresholdGuardian(address(0), 1, members);
+        vm.prank(member1);
+        vm.expectRevert(MERAWalletThresholdGuardian.InvalidWallet.selector);
+        g.proposeEmergencyChange(address(0xE1234), uint64(block.timestamp + 1 days));
+    }
+
+    function test_SetWallet_ZeroAddressReverts() public {
+        address[] memory members = new address[](1);
+        members[0] = member1;
+        MERAWalletThresholdGuardian g = new MERAWalletThresholdGuardian(address(0), 1, members);
+        vm.prank(member1);
+        vm.expectRevert(MERAWalletThresholdGuardian.InvalidWallet.selector);
+        g.setWallet(address(0));
+    }
+
+    function test_Constructor_WithNonZeroWalletSetsWallet() public {
+        address[] memory members = new address[](1);
+        members[0] = member1;
+        MERAWalletThresholdGuardian g = new MERAWalletThresholdGuardian(address(wallet), 1, members);
+        assertEq(g.wallet(), address(wallet));
+    }
+
+    function test_Constructor_DuplicateMemberReverts() public {
+        address[] memory members = new address[](2);
+        members[0] = member1;
+        members[1] = member1;
+        vm.expectRevert(abi.encodeWithSelector(MERAWalletThresholdGuardian.DuplicateMember.selector, member1));
+        new MERAWalletThresholdGuardian(address(0), 1, members);
+    }
+
+    function test_Constructor_ZeroMemberReverts() public {
+        address[] memory members = new address[](1);
+        members[0] = address(0);
+        vm.expectRevert(MERAWalletThresholdGuardian.InvalidMember.selector);
+        new MERAWalletThresholdGuardian(address(0), 1, members);
+    }
+
+    function test_Constructor_ThresholdZeroReverts() public {
+        address[] memory members = new address[](1);
+        members[0] = member1;
+        vm.expectRevert(MERAWalletThresholdGuardian.InvalidThreshold.selector);
+        new MERAWalletThresholdGuardian(address(0), 0, members);
+    }
+
     function test_ExecuteProposal_ChangesEmergencyWhenNReached() public {
         address newEmergency = address(0xE0001);
         bytes32 proposalId = _propose(newEmergency, 1 days, member1);
@@ -68,6 +144,30 @@ contract MERAWalletThresholdGuardianTest is Test {
             abi.encodeWithSelector(MERAWalletThresholdGuardian.AlreadyApproved.selector, proposalId, member1)
         );
         guardian.approveProposal(proposalId);
+    }
+
+    function test_RevokeApproval_NotApprovedReverts() public {
+        bytes32 proposalId = _propose(address(0xE2101), 1 days, member1);
+
+        vm.prank(member1);
+        vm.expectRevert(
+            abi.encodeWithSelector(MERAWalletThresholdGuardian.NotApproved.selector, proposalId, member1)
+        );
+        guardian.revokeApproval(proposalId);
+    }
+
+    function test_RevokeApproval_AfterApprovalSucceeds() public {
+        bytes32 proposalId = _propose(address(0xE2102), 1 days, member1);
+
+        vm.prank(member1);
+        guardian.approveProposal(proposalId);
+
+        vm.prank(member1);
+        guardian.revokeApproval(proposalId);
+
+        (,,,, uint16 approvals,,) = guardian.proposals(proposalId);
+        assertEq(approvals, 0);
+        assertFalse(guardian.hasApproved(proposalId, member1));
     }
 
     function test_Execute_BeforeThresholdReverts() public {
